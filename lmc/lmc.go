e07b932d2dd84bbd8fb7c9c14f72ee630b047778
Chris <chris@clr.is>
2022-09-05 00:56:21 +0100

    Restructuring.

--- lmc/lmc.go
+++ lmc/lmc.go
@@ -3,7 +3,7 @@
 // text-form LMC. (Note: this package is simple enough that it doesn't check for
 // non-trivial things such as >100 mailboxes.)
 //
-// Advisory note
+// # Advisory note
 //
 // Note on memory operations: any newly created mailboxes or labels given in a
 // memory operation must be manually added to any program! During any function
@@ -12,7 +12,9 @@
 // Consider using utility functions provided in *Program, which do this for you.
 package lmc
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Address int64
 type Value int
@@ -28,4 +30,29 @@ type Addressable interface {
 
 type Identifiable interface {
 	Identifier() string
+}
+
+// ---------- Error ----------
+
+type Error struct {
+	error
+	E      error
+	Header string
+	Child  error
+}
+
+func (e *Error) Error() string {
+	var s string
+
+	if e.Header != "" {
+		s = e.Header + " "
+	}
+
+	s += e.E.Error()
+
+	if e.Child != nil {
+		s += ": " + e.Child.Error()
+	}
+
+	return s
 }
